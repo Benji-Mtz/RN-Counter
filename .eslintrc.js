17f6f8fecd717fd0bba894bdee6932787d06f50a
Benji-Mtz <49083076+Benji-Mtz@users.noreply.github.com>
2021-10-25 20:01:32 -0500

    Add Styles

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community/eslint-config',
+    'eslint-config-prettier',
+  ],
+  rules: {
+    "react-native/no-inline-styles": 0,
+    'prettier/prettier': 0,
+  },
+};
